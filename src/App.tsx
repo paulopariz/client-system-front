6cd44065ffe4a15b975b44e937489262300d45fa
paulopariz <paulopariz01@gmail.com>
2023-12-31 23:07:15 -0300

    🚧 layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
-export default function App(){
+export default function App() {
+  return (
+    <div className="bg-blue-primary w-screen h-screen">
+      <main className=" max-w-4xl m-auto px-6 h-36">
+        <header className="pt-14 flex items-center gap-3">
+          <div className="bg-green-500 w-2 h-2 rotate-45 bg-secondary"></div>
+          <h1 className="text-3xl text-white font-semibold tracking-wide">
+            Cadastro
+          </h1>
+        </header>
 
-  return(
-    <div>
+        <form className="mt-10 grid gap-8">
+          <div className="grid gap-2">
+            <label htmlFor="name" className="text-white font-medium">
+              Nome
+            </label>
+            <input
+              id="name"
+              type="text"
+              placeholder="Nome do cliente"
+              className="py-3 px-4 rounded-full outline-none bg-transparent border-blue-950 border text-white focus:border-blue-900 hover:border-blue-900 transition-all placeholder:transition-all placeholder:text-gray-300 font-light text-sm focus:placeholder:pl-2 focus:placeholder:text-gray-400"
+            />
+          </div>
 
-      <h1 className="text-3xl text-zinc-800">Hello</h1>
+          <div className="grid gap-2">
+            <label htmlFor="email" className="text-white font-medium">
+              E-mail
+            </label>
+            <input
+              id="email"
+              type="text"
+              placeholder="E-mail do cliente"
+              className="py-3 px-4 rounded-full outline-none bg-transparent border-blue-950 border text-white focus:border-blue-900 hover:border-blue-900 transition-all placeholder:transition-all placeholder:text-gray-300 font-light text-sm focus:placeholder:pl-2 focus:placeholder:text-gray-400"
+            />
+          </div>
+        </form>
+      </main>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-primary": "#08031A",
+        "blue-secondary": "#292E4B",
+      },
+    },
   },
   plugins: [],
 };
